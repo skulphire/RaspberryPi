e003f47ea0222138a9e7f420d2708420b9582764
Creghton <cdkrobotics@gmail.com>
2016-12-31 14:43:57 -0500

    Edited

--- Project/Drivers/MPU6050/mpudata.py
+++ Project/Drivers/MPU6050/mpudata.py
@@ -26,3 +26,15 @@ class MpuData(object):
         self.gyroX=x/131
         self.gyroY=y/131
         self.gyroZ=z/131
+
+    #acc
+    def getAccXYZ(self):
+        #raw values
+        x = self.mpu.read_word_2c(0x3b)
+        y = self.mpu.read_word_2c(0x3d)
+        z = self.mpu.read_word_2c(0x3f)
+
+        #scaled values
+        self.accX = x/16384.0
+        self.accY = y/16384.0
+        self.accZ = z/16384.0
