dff5926d1bb30b2975bd8242c97618c7e01a8f3f
Creghton <cdkrobotics@gmail.com>
2017-01-02 18:55:32 -0500

    Working on motor movement

--- projects/drivers/humanoid/arms.py
+++ projects/drivers/humanoid/arms.py
@@ -7,7 +7,7 @@ class Arms(object):
         self.pwm = Adafruit_PCA9685.PCA9685(0x41)
         self.pwm.set_pwm_freq(60)
         self.servoMin = 100
-        self.servoMax = 600
+        self.servoMax = 500
     def leftShoulder(self, degrees):
         if degrees == 0:
             self.pwm.set_pwm(0,0,self.servoMin)
