99ff37bbe9330fdd2725a11a40e01a06b550c1a3
Creghton <cdkrobotics@gmail.com>
2017-01-02 22:12:19 -0500

    Working on motor movement

--- projects/drivers/sets/positions.py
+++ projects/drivers/sets/positions.py
@@ -6,7 +6,11 @@ class Pos(object):
     def __init__(self):
         self.arm = Arms()
 
-    def start(self):
+    def stop(self):
+        time.sleep(3)
+        self.arm.servosOff()
+
+    def initial(self):
         # standard position:
         self.arm.leftShoulder(0)
         self.arm.leftElbow(0)
@@ -15,7 +19,3 @@ class Pos(object):
         self.arm.rightShoulder(0)
         self.arm.rightElbow(0)
         self.arm.rightHand(90)
-
-        #dont need later
-        time.sleep(3)
-        self.arm.servosOff()
--- projects/main.py
+++ projects/main.py
@@ -1,17 +1,16 @@
 import time
 from drivers.sets.positions import Pos
+from drivers.mpu6050.mpudata import MpuData
+
+
 #usefull commands
 #i2cdetect -y 1
 
 if __name__ == '__main__':
+    mpu = MpuData()
     position = Pos()
-    position.start()
+    position.initial()
+    while True:
+        mpu.printValues('b')
+        time.sleep(1)
 
-    #servo = Translator()
-    #servo.balance()
-
-#    mpu = MpuData()
-#    mpu.turnOn(0x68)
-#    while True:
-#        mpu.printValues('b')
-#        time.sleep(0.5)
