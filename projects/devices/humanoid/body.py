d29065e6019eee69b41e56c176b0213e7dd57181
Creghton <cdkrobotics@gmail.com>
2017-01-03 21:36:15 -0500

    Working on motor movement

--- projects/devices/humanoid/body.py
+++ projects/devices/humanoid/body.py
@@ -23,7 +23,7 @@ class Body(object):
     def leftThighX(self,degrees):
         if degrees == 0:
             self.bpwm.set_pwm(14,0,500)
-        elif degrees == 0:
+        elif degrees == 180:
             self.bpwm.set_pwm(14,0,150)
 
     def rightThighX(self,degrees):
