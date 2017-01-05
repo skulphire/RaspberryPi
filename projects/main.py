54ec620e60d30213d42e303861a8c67b52558313
Creghton <cdkrobotics@gmail.com>
2017-01-04 22:23:32 -0500

    Working on motor movement

--- projects/main.py
+++ projects/main.py
@@ -11,14 +11,16 @@ if __name__ == '__main__':
    position = Pos()
    arm = Arms()
    #position.new()
-   arm.testerRightShoulder(180,4)
-   arm.testerRightShoulder(90, 4)
-   arm.testerRightShoulder(60, 4)
-   arm.testerRightShoulder(150, 4)
-   arm.testerRightShoulder(20, 4)
+   while True:
+      arm.testerRightShoulder(180,4)
+      arm.testerRightShoulder(90, 4)
+      arm.testerRightShoulder(60, 4)
+      arm.testerRightShoulder(150, 4)
+      arm.testerRightShoulder(20, 4)
+      arm.testerRightShoulder(0, 4)
 
-   time.sleep(5)
-   position.stop()
+   #time.sleep(5)
+   #position.stop()
 
 
     #while True:
