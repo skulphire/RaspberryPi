94c20aa4c9ca4f9d409bff4afd4821497b2291b1
Creghton <cdkrobotics@gmail.com>
2017-01-04 23:33:02 -0500

    Working on motor movement

--- projects/drivers/sets/positions.py
+++ projects/drivers/sets/positions.py
@@ -15,11 +15,11 @@ class Pos(object):
         self.arm.servosOff()
     def initial(self):
         # standard position:
-        self.arm.leftShoulder(180,5)
+        self.arm.leftShoulder(90,5)
         self.arm.leftElbow(180,5)
         self.arm.leftHand(90,5)
 
-        self.arm.rightShoulder(0,5)
+        self.arm.rightShoulder(90,5)
         self.arm.rightElbow(0,5)
         self.arm.rightHand(90,5)
 
