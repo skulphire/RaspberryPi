90653ccd8e183c7aa30b0c47e05725dbb3c671ca
Creghton <cdkrobotics@gmail.com>
2017-01-04 22:20:54 -0500

    Working on motor movement

--- projects/devices/humanoid/arms.py
+++ projects/devices/humanoid/arms.py
@@ -73,7 +73,7 @@ class Arms(object):
                 if speed == 4:
                     for x in range(self.trsLastPulse,End,to):
                         self.tpwm.set_pwm(3,0,x)
-                        time.sleep(0.2)
+                        time.sleep(0.1)
                         print(x)
         self.trsLastPulse = pulse
 
