2564d8ee07c50f30714f73d9ac5e90ab6b056122
Creghton <cdkrobotics@gmail.com>
2017-01-06 22:28:42 -0500

    Working on motor movement

--- projects/drivers/servos/runservos.py
+++ projects/drivers/servos/runservos.py
@@ -21,7 +21,7 @@ class RunServos(object):
             pulse = pulseDict[channel][x]
             self.last = pulse
         except:
-            pulse = 0
+            pulse = -1
             pass
         return pulse
     def servos(self ,speed, pulseDict,controller):
