ad2fc87290dcf353a311dee11b47325065792380
Creghton <cdkrobotics@gmail.com>
2017-01-06 19:47:09 -0500

    Working on motor movement

--- projects/drivers/servos/runservos.py
+++ projects/drivers/servos/runservos.py
@@ -14,7 +14,6 @@ class RunServos(object):
         self.channels =[0]
 
     def testlist(self,x, pulseDict, channel):
-        print("yep")
         try:
             pulse = pulseDict[channel][x]
         except:
