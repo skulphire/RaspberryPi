411d198da8292b4dfa2a3c07b92e4cecc49084f7
Creghton <cdkrobotics@gmail.com>
2017-01-07 18:28:55 -0500

    Working on motor movement

--- projects/devices/humanoid/body.py
+++ projects/devices/humanoid/body.py
@@ -17,7 +17,7 @@ class Body(object):
 
     def waist(self,degrees):
         if degrees % 5 == 0:
-            pulse = (degrees * 2.5) + 200
+            pulse = (degrees * 2.5) + 250
             pulse = int(pulse)
             if pulse < self.wLastPulse:
                 End = pulse - 1
