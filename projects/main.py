cd32571fb40015575004abb6fde926c200e54992
Creghton <cdkrobotics@gmail.com>
2017-01-06 22:00:53 -0500

    Working on motor movement

--- projects/main.py
+++ projects/main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
    arm.commit()
    time.sleep(.5)
    #arm.leftShoulder(180, 5)
-   #arm.leftElbow(80, 5)
+   arm.leftElbow(80, 5)
    arm.leftHand(90, 5)
    arm.commit()
    #arm.rightShoulder(180,5)
