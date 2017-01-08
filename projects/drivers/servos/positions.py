545ce1efbd4ca751cca1b8add504f6dc2a6b68e2
Creghton <cdkrobotics@gmail.com>
2017-01-08 02:41:53 -0500

    Working on motor movement

--- projects/drivers/servos/positions.py
+++ projects/drivers/servos/positions.py
@@ -95,7 +95,7 @@ class Pos(object):
 
         self.legs.rightknee(60)
         self.legs.rightankleY(90)
-        self.legs.rightankleX(180)
+        self.legs.rightankleX(120)
         self.commit()
 
 
