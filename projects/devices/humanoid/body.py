1a6d00332f3c91ecd0ff216e02f1a3d21d9e2ac1
Creghton <cdkrobotics@gmail.com>
2017-01-08 00:49:14 -0500

    Working on motor movement

--- projects/devices/humanoid/body.py
+++ projects/devices/humanoid/body.py
@@ -86,7 +86,7 @@ class Body(object):
 
     def leftThighX(self, degrees):
         if degrees % 5 == 0:
-            pulse = (degrees * 2.5) +75
+            pulse = (degrees * 1.8) +30
             pulse = int(pulse)
             if pulse < self.ltxLastPulse:
                 End = pulse - 1
