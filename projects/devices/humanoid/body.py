8b28dc253c2295e737fdf8faccd9772b26a7e7af
Creghton <cdkrobotics@gmail.com>
2017-01-07 19:07:07 -0500

    Working on motor movement

--- projects/devices/humanoid/body.py
+++ projects/devices/humanoid/body.py
@@ -6,12 +6,12 @@ class Body(object):
     def __init__(self):
         self.pulsesDict = PULSESDICT
         self.wLastPulse = 0 #12
-        self.ltxLastPulse = 0 #11
-        self.rtxLastPulse = 0 #10
-        self.ltyLastPulse = 0 #9
-        self.rtyLastPulse = 0 #8
-        self.lhipLastPulse = 0 #7
-        self.rhipLastPulse = 0 #6
+        self.rtxLastPulse = 0 #11
+        self.rtyLastPulse = 0 #10
+        self.rhipLastPulse = 0 #9
+        self.ltxLastPulse = 0 #8
+        self.ltyLastPulse = 0 #7
+        self.lhipLastPulse = 0 #6
 
 
 
@@ -37,51 +37,51 @@ class Body(object):
         if degrees % 5 == 0:
             pulse = (degrees * 2.5) + 150
             pulse = int(pulse)
-            if pulse < self.ltxLastPulse:
+            if pulse < self.rtxLastPulse:
                 End = pulse - 1
                 step = -25
             else:
                 End = pulse + 1
                 step = 25
             self.pulsesDict.setdefault(11,[])
-            for x in range(self.ltxLastPulse, End, step):
+            for x in range(self.rtxLastPulse, End, step):
                 self.pulsesDict[11].append(x)
             CHANNELS.append(11)
-            self.ltxLastPulse = pulse
+            self.rtxLastPulse = pulse
             CONTROLLER.append(1)
 
     def rightthighY(self, degrees):
         if degrees % 5 == 0:
             pulse = (degrees * 2.5) + 50
             pulse = int(pulse)
-            if pulse < self.rtxLastPulse:
+            if pulse < self.rtyLastPulse:
                 End = pulse - 1
                 step = -25
             else:
                 End = pulse + 1
                 step = 25
             self.pulsesDict.setdefault(10, [])
-            for x in range(self.rtxLastPulse, End, step):
+            for x in range(self.rtyLastPulse, End, step):
                 self.pulsesDict[10].append(x)
             CHANNELS.append(10)
-            self.rtxLastPulse = pulse
+            self.rtyLastPulse = pulse
             CONTROLLER.append(1)
 
     def righthip(self, degrees):
         if degrees % 5 == 0:
             pulse = (degrees * 2.5) + 50
             pulse = int(pulse)
-            if pulse < self.rtxLastPulse:
+            if pulse < self.rhipLastPulse:
                 End = pulse - 1
                 step = -25
             else:
                 End = pulse + 1
                 step = 25
             self.pulsesDict.setdefault(9, [])
-            for x in range(self.rtxLastPulse, End, step):
+            for x in range(self.rhipLastPulse, End, step):
                 self.pulsesDict[9].append(x)
             CHANNELS.append(9)
-            self.rtxLastPulse = pulse
+            self.rhipLastPulse = pulse
             CONTROLLER.append(1)
 
         #if degrees == 0:
