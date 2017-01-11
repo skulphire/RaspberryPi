1f2d8415576309bb39eb548c262f6a47199d51a0
Creghton <cdkrobotics@gmail.com>
2017-01-10 19:13:38 -0500

    Working on motor movement

--- projects/devices/humanoid/body.py
+++ projects/devices/humanoid/body.py
@@ -109,7 +109,7 @@ class Body(object):
 
     def leftthighY(self,degrees):
         if degrees % 5 == 0:
-            pulse = (degrees * 2.5) +100
+            pulse = -(degrees * 2.5) +600
             pulse = int(pulse)
             if pulse < self.ltyLastPulse:
                 End = pulse - 1
